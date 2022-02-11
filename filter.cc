90f714171fe08337d4625a67f741fa98a23640c8
Thalles Nonato <eusouthalles@gmail.com>
2022-02-11 00:26:19 -0300

    Filter.cc (100/100)

--- filter.cc
+++ filter.cc
@@ -1,5 +1,6 @@
 #include <type_traits>
 #include <algorithm>
+#include <functional>
 
 using namespace std;
 template<typename T1,typename T2>
@@ -9,12 +10,24 @@ auto operator | (const T1 &list , T2 callback){
         std::vector<decltype(type)> resposta;
         copy_if(begin(list), end(list),back_inserter(resposta), callback);
         return resposta;
-
-    } else if constexpr (std::is_same<decltype(callback(*std::begin(list))), void>::value) {for_each(begin(list),end(list),callback);}
-
-    else{
+    }else if constexpr (std::is_void<decltype(callback(*std::begin(list)))>::value) {for_each(begin(list),end(list),callback);}
+    
+    else if constexpr (std::is_function<decltype(callback)>::value){
         vector<decltype(callback(*std::begin(list)))> resposta;
         transform(*std::begin(list),*std::end(list),back_inserter(resposta),callback);
         return resposta;
+    }    
+    else{
+        std::vector<decltype(callback(*std::begin(list)))> resposta;
+        for(const auto &x: list)
+            resposta.push_back(callback(x));
+        return resposta;
     }
+}
+template<typename T1>
+auto operator |(const T1 &list, decltype(&std::string::length) callbackMethod){
+    std::vector<decltype(&std::string::length)> resposta;
+    for(const auto &x: list)
+        resposta.push_back(std::invoke(callbackMethod, x));
+    return resposta;    
 }
