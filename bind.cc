3a02faca8816914fd79f5efcf62ac6806d905d4d
Thalles Nonato <eusouthalles@gmail.com>
2022-02-17 14:42:05 -0300

    Bind (100/100)

--- bind.cc
+++ bind.cc
@@ -1,8 +1,7 @@
 #include <iostream>
 #include <functional>
 
-using std::is_invocable_v, std::invoke, std::is_class_v, std::cout;
-
+using std::invoke, std::is_class_v, std::cout, std::is_invocable_v;
 
 template<typename Function>
 struct Bind{
@@ -23,15 +22,12 @@ struct Bind{
     Function f;
 };
 
-
 template<typename Function>
 auto bind(Function f){
     return Bind{f};
 }
 
-
 template<typename Function,typename ...Args>
-
 auto bind(Bind<Function> f, Args ...args){
      if constexpr(is_invocable_v<Function,Args...>)
                 return invoke(f->f, args...);
@@ -44,16 +40,10 @@ template<typename Function, typename ...Args>
 auto bind(Function f, Args ...args){
      if constexpr(is_invocable_v<Function,Args...>)
                 return invoke(f, args...);
-            else{
+    else if constexpr (is_class_v<Function>) {
+            return [&f, args...](auto...resto){ return invoke(f, args..., resto...); };
+        }
+        else{
                 return [f, args...](auto ...resto){ return invoke(f,args...,resto...); };
             }
-}
-
-int mdc( int a, int b ) { return b == 0 ? a : mdc( b, a%b ); };
-
-int main(){
-    auto f2 = bind( mdc );
-    auto f1 = bind( f2, 12 );
-    for( int i = 2; i <= 12; i++ )
-        cout << f1( i ) <<  " ";
 }
